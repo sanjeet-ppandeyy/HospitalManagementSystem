846056e95addc3420bca736838669ace134cedb9
sanjeet-ppandeyy <sanjeetppandeyy@gmail.com>
2025-05-27 10:53:05 +0530

    Update conn.java

--- conn.java
+++ conn.java
@@ -1,1 +1,19 @@
-package HospitalManagementSystem;import java.sql.Connection;import java.sql.DriverManager;import java.sql.Statement;public class conn {    public Connection connection;    public Statement statement;    public conn() {        try {            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_management_system", "root", "@Babajii123");            statement = connection.createStatement();        } catch (Exception e) {            e.printStackTrace();        }    }}
+package HospitalManagementSystem;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class conn {
+    public Connection connection;
+    public Statement statement;
+
+    public conn() {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hospital_management_system", "root", "@Babajii123");
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
